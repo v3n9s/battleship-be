03f4b75e5223a59abd03c5157c70f24f39541846
Venyamin Shvets <venyashvets@gmail.com>
2022-11-21 16:41:36 +0300

    build: configure typescript, babel and webpack

---
+++ webpack.config.ts
@@ -0,0 +1,34 @@
+import { Configuration } from 'webpack';
+import NodemonPlugin from 'nodemon-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+export default (env: { [k: string]: string | boolean }): Configuration => {
+  const isDev = env.mode === 'dev';
+
+  return {
+    mode: isDev ? 'development' : 'production',
+    entry: {
+      index: './src/index',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/i,
+          use: ['babel-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '...'],
+    },
+    target: 'node',
+    watch: !!isDev,
+    devtool: isDev ? 'inline-source-map' : false,
+    plugins: [new NodemonPlugin(), new ForkTsCheckerWebpackPlugin()],
+    externals: [nodeExternals()],
+    output: {
+      clean: true,
+    },
+  };
+};
