237959ad81e23a3a9644db0114c851ddf608e0e3
Venyamin Shvets <venyashvets@gmail.com>
2022-12-27 19:27:43 +0300

    feat: implement schemas for new client messages

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -5,6 +5,9 @@ import {
   CreateRoomMessage,
   JoinRoomMessage,
   LeaveRoomMessage,
+  ReadyGameMessage,
+  ReadyRoomMessage,
+  SetPositionsMessage,
   UserDto,
 } from '../types';
 
@@ -70,11 +73,63 @@ export const leaveRoomMessageSchema: JSONSchemaType<LeaveRoomMessage> = {
   required: ['id'],
 };
 
+export const readyRoomMessageSchema: JSONSchemaType<ReadyRoomMessage> = {
+  type: 'object',
+  properties: {
+    roomId: {
+      type: 'string',
+      minLength: 36,
+      maxLength: 36,
+    },
+  },
+  required: ['roomId'],
+};
+
+export const setPositionsMessageSchema: JSONSchemaType<SetPositionsMessage> = {
+  type: 'object',
+  properties: {
+    roomId: {
+      type: 'string',
+      minLength: 36,
+      maxLength: 36,
+    },
+    positions: {
+      type: 'array',
+      minItems: 10,
+      maxItems: 10,
+      items: {
+        type: 'array',
+        minItems: 10,
+        maxItems: 10,
+        items: {
+          type: 'boolean',
+        },
+      },
+    },
+  },
+  required: ['roomId', 'positions'],
+};
+
+export const readyGameMessageSchema: JSONSchemaType<ReadyGameMessage> = {
+  type: 'object',
+  properties: {
+    roomId: {
+      type: 'string',
+      minLength: 36,
+      maxLength: 36,
+    },
+  },
+  required: ['roomId'],
+};
+
 export const ClientMessageValidatonFuncs = Object.fromEntries(
   Object.entries({
     CreateRoom: createRoomMessageSchema,
     JoinRoom: joinRoomMessageSchema,
     LeaveRoom: leaveRoomMessageSchema,
+    ReadyRoom: readyRoomMessageSchema,
+    SetPositions: setPositionsMessageSchema,
+    ReadyGame: readyGameMessageSchema,
   } satisfies {
     [K in keyof ClientMessages]: JSONSchemaType<ClientMessages[K]>;
   }).map(([k, v]) => [k, ajv.compile(v)]),
